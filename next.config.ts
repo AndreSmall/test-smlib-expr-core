487d13a3d016ee265a91785d6839ee0bada7da47
Andre Small <asmall@surveymonkey.com>
2025-01-02 13:04:09 -0500

    updated next publish config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "smlibexpr-core-test-pages"
+  basePath: "/test-smlib-expr-core"
+  // output: "export"
 };
 
 export default nextConfig;
